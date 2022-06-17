34bf2589107cfb13f36ba35a15be377dbb277786
OG <gauch_oleg@ukr.net>
2022-06-17 10:37:34 +0300

    update read me

--- config/params_example.php
+++ config/params_example.php
@@ -4,6 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    'parseUrl' => 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange',
-    'token' => 'VNVUgk099qNN49VJFUHYeErX5RBk5wTXZd_171ur'
+    'token' => 'VNVUgk099qNN49VJFUHYeErX5RBk5wTXZd_711ur'
 ];
